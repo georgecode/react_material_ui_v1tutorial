f9dfd1e1b2b82dd99812af776c76a92a8411234a
georgecode <georgeczurlanis@gmail.com>
2018-05-15 21:52:52 -0700

    Add Exercise app skeleton

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,15 @@
 import React, { Component, Fragment } from 'react';
-import Header from './Layouts/Header';
-import Footer from './Layouts/Footer';
+import { Header, Footer} from './Layouts';
+import Exercises from './Exercises';
 
-// export default props =>
-// 	<div>
-// 		<h1>Hello from react</h1>
-// 	</div>
+
 export default class extends Component{
 	render(){
 		return <Fragment>
 		<Header />
 		<h1>Hello this is an app</h1>
+
+		<Exercises />
 		<Footer />
 		</Fragment>
 	}
---
+++ src/Components/Exercises/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default ()=>
+<div>
+	<h1>Exercises</h1>
+</div>
+	
